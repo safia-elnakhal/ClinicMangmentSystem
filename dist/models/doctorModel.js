3cd34dd1c45b73dadaf51d7c69077995afb52154
safia elnakhal <safiaelnakhal@gmail.com>
2022-07-16 16:52:55 +0200

    add model==>doctor and address

--- dist/models/doctorModel.js
+++ dist/models/doctorModel.js
@@ -1,1 +1,23 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const doctorSchema = new mongoose_1.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+    },
+    address: {
+        type: mongoose_1.Schema.Types.ObjectId, ref: "address"
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+    },
+});
+module.exports = (0, mongoose_1.model)("doctors", doctorSchema);
---
+++ src/models/doctorModel.ts
@@ -0,0 +1,34 @@
+import { Schema, model, Types } from "mongoose";
+
+interface IDoctor {
+    name: string;
+    age: number;
+    address:Types.ObjectId;
+    email: string;
+    password: string;
+    speciality: string;
+}
+
+const doctorSchema =new Schema <IDoctor>({
+    name: {
+      type: String,
+      required: true,
+      },
+      age:{
+          type:Number,
+          
+      },
+address: {
+    type:Schema.Types.ObjectId ,ref:"address"
+},
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+    },
+   
+  });
+
+  module.exports = model<IDoctor>("doctors",doctorSchema)
