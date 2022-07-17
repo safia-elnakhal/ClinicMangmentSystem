20774a9f085246b56b72516d3f69077546ddf338
safia elnakhal <safiaelnakhal@gmail.com>
2022-07-18 01:37:31 +0200

    update ==>doctor route

---
+++ src/routes/doctorRoutes.ts
@@ -0,0 +1,25 @@
+
+import { Router } from "express"
+import * as Controller from "../controllers/doctorcontroller";
+const routes = Router();
+
+routes
+  .route("/doctor")
+  .get(Controller.getAllDoctor)
+  .post(
+      Controller.createDoctor
+  )
+  .put(
+    Controller.updateDoctor
+  );
+
+  routes
+  .route("/doctor/:id")
+  .get(
+    Controller.getDoctorByID
+  )
+  .delete(
+    Controller.deleteDoctor
+  );
+
+export default routes;
