dba50749d08a00cdbc7e6fd361e988e650e12a5b
EngMostafaHussien <108274816+EngMostafaHussien@users.noreply.github.com>
2022-07-19 01:24:22 +0200

    waiting edit

--- src/routes/loginRoute.ts
+++ src/routes/loginRoute.ts
@@ -1,9 +1,9 @@
-import express from "express";
-import * as controller from "../controllers/loginController";
-const route = express.Router();
+// import express from "express";
+// import * as controller from "../controllers/loginController";
+// const route = express.Router();
 
-route.post("/login/doctor", controller.loginDoctor);
-route.post("/login/employee", controller.loginEmployee);
-route.post("/login", controller.loginPatient);
+// route.post("/login/doctor", controller.loginDoctor);
+// route.post("/login/employee", controller.loginEmployee);
+// route.post("/login", controller.loginPatient);
 
-export default route;
+// export default route;
