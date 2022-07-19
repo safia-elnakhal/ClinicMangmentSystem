a3b39f78ecc386fbb3ddeb3b36fa87b10337e15c
Mahmoud Samy <mahmoudsamy50@outlook.com>
2022-07-19 23:55:33 +0200

    added invoice model

---
+++ src/models/invoiceModel.ts
@@ -0,0 +1,28 @@
+import { Schema, Types, model, Document } from 'mongoose'
+
+interface IInvoice extends Document {
+    doctorId: Types.ObjectId
+    patientId: Types.ObjectId
+    Charge: number
+}
+
+const InvoiceSchema = new Schema<IInvoice>({
+    doctorId: {
+        type: Schema.Types.ObjectId,
+        ref: 'doctors',
+        required: true,
+    },
+    patientId: {
+        type: Schema.Types.ObjectId,
+        ref: 'patients',
+        required: true,
+    },
+    Charge: {
+        type: Schema.Types.Number,
+        required: true,
+    },
+})
+
+const Invoice = model<IInvoice>('Patient', InvoiceSchema)
+
+export { Invoice, IInvoice }
