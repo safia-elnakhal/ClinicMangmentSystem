a10bd37823c97d10eea40feca549b1dad1f739a6
Mahmoud Samy <mahmoudsamy50@outlook.com>
2022-07-19 22:09:20 +0200

    formatted Radwa's code

--- src/routes/patientRoute.ts
+++ src/routes/patientRoute.ts
@@ -1,48 +1,72 @@
-const { body, param, query } = require("express-validator");
-import { Router } from "express"
-import * as patientController from "../controllers/patientController";
-const patientRoute = Router();
-import validationMW from "../middlewares/validationMw"
+const { body, param, query } = require('express-validator')
+import { Router } from 'express'
+import * as patientController from '../controllers/patientController'
+const patientRoute = Router()
+import validationMW from '../middlewares/validationMw'
 
+patientRoute
+    .route('/patient')
+    .get(patientController.getAllPatients)
+    .post(
+        [
+            body('name').isAlpha().withMessage('patient name should be string'),
+            body('email')
+                .isEmail()
+                .withMessage('patient Email should be Email'),
+            body('password')
+                .isLength({ min: 6 })
+                .withMessage('patient password should be more than 6 numbers'),
+            body('age').isNumeric().withMessage('patient age should be Email'),
+            body('historyOfDisease')
+                .isAlpha()
+                .withMessage('patient history with disease should be string'),
+            body('InsuranceCompany')
+                .isAlpha()
+                .withMessage('patient InsuranceCompany should be string'),
+            body('gender')
+                .isAlpha()
+                .withMessage('patient gender should be string'),
+        ],
+        validationMW,
+        patientController.createPatient
+    )
 
-patientRoute.route("/patient")
-  .get(patientController.getAllPatients)
-  .post([
-    body("name").isAlpha().withMessage("patient name should be string"),
-    body("email").isEmail().withMessage("patient Email should be Email"),
-    body("password").isLength({ min: 6 }).withMessage("patient password should be more than 6 numbers"),
-    body("age").isNumeric().withMessage("patient age should be Email"),
-    body("historyOfDisease").isAlpha().withMessage("patient history with disease should be string"),
-    body("InsuranceCompany").isAlpha().withMessage("patient InsuranceCompany should be string"),
-    body("gender").isAlpha().withMessage("patient gender should be string"),
+    .put(
+        [
+            body('name').isAlpha().withMessage('patient name should be string'),
+            body('email')
+                .isEmail()
+                .withMessage('patient Email should be Email'),
+            body('password')
+                .isLength({ min: 6 })
+                .withMessage('patient password should be more than 6 numbers'),
+            body('age').isNumeric().withMessage('patient age should be Email'),
+            body('historyOfDisease')
+                .isAlpha()
+                .withMessage('patient history with disease should be string'),
+            body('InsuranceCompany')
+                .isAlpha()
+                .withMessage('patient InsuranceCompany should be string'),
+            body('gender')
+                .isAlpha()
+                .withMessage('patient gender should be string'),
+        ],
+        validationMW,
+        patientController.updatePatient
+    )
 
-  ], validationMW,
-    patientController.createPatient)
+patientRoute
+    .route('/patient/:id')
+    .delete(
+        [param('id').isMongoId().withMessage('patient id should be objectId')],
+        validationMW,
+        patientController.deletePatientById
+    )
 
+    .get(
+        [param('id').isMongoId().withMessage('patient id should be objectId')],
+        validationMW,
+        patientController.getPatientsById
+    )
 
-  .put([
-    body("name").isAlpha().withMessage("patient name should be string"),
-    body("email").isEmail().withMessage("patient Email should be Email"),
-    body("password").isLength({ min: 6 }).withMessage("patient password should be more than 6 numbers"),
-    body("age").isNumeric().withMessage("patient age should be Email"),
-    body("historyOfDisease").isAlpha().withMessage("patient history with disease should be string"),
-    body("InsuranceCompany").isAlpha().withMessage("patient InsuranceCompany should be string"),
-    body("gender").isAlpha().withMessage("patient gender should be string"),
-  ], validationMW,
-    patientController.updatePatient)
-
-patientRoute.route("/patient/:id")
-  .delete([param("id").isMongoId().withMessage("patient id should be objectId")
-  ], validationMW,
-    patientController.deletePatientById)
-
-
-  .get([param("id").isMongoId().withMessage("patient id should be objectId")
-  ], validationMW,
-    patientController.getPatientsById)
-
-
-
-
-
-export default patientRoute;
+export default patientRoute
