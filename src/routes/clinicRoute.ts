ee4ef47ad58458ad72f6be2398799a7151b0fee2
maryamragab <mariam.relshafei@yahoo.com>
2022-07-20 10:43:26 +0200

    adding some validations

--- src/routes/clinicRoute.ts
+++ src/routes/clinicRoute.ts
@@ -1,16 +1,53 @@
-import express from 'express'
-import clinicController from '../controllers/clinicController'
+import { Router } from 'express'
+import { param, body } from 'express-validator'
 
-const router = express.Router()
+import {
+    createClinic,
+    getAllClinicData,
+    getAllClinicServices,
+    updateClinicServices,
+    getClinicServiceById,
+    deleteClinicServices,
+} from '../controllers/clinicController'
+import validationMw from '../middlewares/validationMw'
+
+const router = Router()
 
 router
     .route('/clinic')
-    .post(clinicController.createClinicService)
-    .get(clinicController.getAllClinicServices)
+    .post(
+        [
+            body('name')
+                .isAlpha('en-US', { ignore: '' })
+                .withMessage('Service Name Must Be Characters'),
+        ],
+        validationMw,
+        createClinic
+    )
+    .get(getAllClinicData)
 
 router
     .route('/clinic/services')
-    .put(clinicController.updateClinicServices)
-    .delete(clinicController.deleteClinicServices)
+    .get(getAllClinicServices)
+    .put(
+        [
+            body('services.name') //! adding unique
+                // .isEmpty()
+                // .withMessage('Service Name Should Be Added')
+                .isAlpha('en-US', { ignore: 's' })
+                .withMessage('Service Name Must Be Characters'),
+            body('services.description')
+                .isAlpha('en-US', { ignore: 's-.,;?' })
+                .withMessage('Service Description Must Be Characters'),
+            // body('services.*.doctorId')
+            //     .isMongoId()
+            //     .withMessage('DoctorId Must Be Of ObjectId Datatype '),
+        ],
+        validationMw,
+        updateClinicServices
+    )
+    .delete(deleteClinicServices)
 
-export = router
+router.get('/clinic/services/:id', getClinicServiceById)
+
+export default router
