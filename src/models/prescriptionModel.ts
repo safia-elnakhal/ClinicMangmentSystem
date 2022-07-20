0b295161c41092ff1f57c3d597de45d287b790cf
Mahmoud Samy <mahmoudsamy50@outlook.com>
2022-07-20 12:50:51 +0200

    added prescription model

---
+++ src/models/prescriptionModel.ts
@@ -0,0 +1,51 @@
+// var mongoose = require('mongoose');
+// var Schema = mongoose.Schema;
+// var Prescription = new Schema({
+//   medicineId: {
+//     type: Medicine,
+//     required: true
+//   },
+//   doctoId: {
+//     type: String
+//   },
+//   patientId: {
+//     type: String
+//   },
+//   Consultation: {
+//     type: Date
+//   }
+// });
+
+import { Schema, Types, model, Document } from 'mongoose'
+
+interface IPrescription extends Document {
+    medicineId: Types.ObjectId
+    doctorId: Types.ObjectId
+    patientId: Types.ObjectId
+    consultation: Date
+}
+
+const PrescriptionSchema = new Schema<IPrescription>({
+    doctorId: {
+        type: Schema.Types.ObjectId,
+        ref: 'doctors',
+        required: true,
+    },
+    medicineId: {
+        type: Schema.Types.ObjectId,
+        ref: 'medicine',
+        required: true,
+    },
+    patientId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Patient',
+        required: true,
+    },
+    consultation: {
+        type: Schema.Types.Date,
+    },
+})
+
+const Prescription = model<IPrescription>('prescription', PrescriptionSchema)
+
+export { Prescription, IPrescription }
